df5aa39a1900be4cde5f993d33e51904d14f58cc
teal-oyunsuren17 <teal.2022.oyunsuren.purevsuren@gmail.com>
2023-02-06 11:18:42 +0800

    add at home

---
+++ JS-practice/JS-condition/huvisagch.js
@@ -0,0 +1,19 @@
+const a = 85;
+const b = 75;
+const c = 96;
+const d = 69;
+let sum = 0;
+
+if (a >= 80) {
+  sum += a;
+}
+if (b >= 80) {
+  sum += b;
+}
+if (c >= 80) {
+  sum += c;
+}
+if (d >= 80) {
+  sum += d;
+}
+console.log(sum);
